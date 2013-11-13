694414d0dbb3927d07dbbcf487ac46ce80ff40d8
prithvisekhar <prithvisekhar@gmail.com>
2013-11-13 15:28:02 +0000

    adding launch

--- mrs_motors/scripts/i2c_Motor.py
+++ mrs_motors/scripts/i2c_Motor.py
@@ -20,31 +20,61 @@ class I2CInterface():
   def __init__(self, pi_version = 1):
     self.bus = smbus.SMBus(pi_version)
 
-  def writeNumber(self, value):
-    self.bus.write_byte(self.address, value)
-   # bus.write_byte_data(address, 0, value)
+  def writeNumber(self, slave, command):
+    self.bus.write_i2c_block_data(slave,command[0],command[1:len(command)])
+    #self.bus.write_byte(self.address, value)
+   
     return 0
 
-  def readNumber(self):
-    number = self.bus.read_byte(self.address)
+  def readNumber(self,slave,command,numByte):
+    number = self.bus.read_i2c_block_data(slave, command)[:numByte]
+    #self.bus.read_byte()
     # number = bus.read_byte_data(address, 1)
     return number
 
   def execute(self):
     while True:
         #var = []
-        var = raw_input("Enter Slave Address 1-9: ")
-        if not var:
-            continue
-        self.address=var
-        writeNumber(var)
-        print "RPI: Hi Arduino, I sent you ", var
+        self.cmd=[]
+        self.address = 4
+        #~ input("Slave Address (4-15): ")
+        #~ if not self.address:
+            #~ continue
+        self.cmd.append(self.address)
+        
+        self.byte1 = 3
+        #~ input("1st Value 1-9: ")
+        #~ if not self.byte1:
+            #~ continue
+        self.cmd.append(self.byte1)
+        
+        self.byte2 = 3
+        #~ input("2nd Value 1-9: ")
+        #~ if not self.byte2:
+            #~ continue
+        self.cmd.append(self.byte2)
+        
+        self.bus.write_byte(self.address, self.byte1)
+        #~ self.writeNumber(self.address, self.cmd)
+        print "RPI: Hi Arduino, I sent you ", self.cmd
         # sleep one second
         time.sleep(1)
     
-        number = readNumber()
-        print "Arduino: Hey RPI, I received a digit ", number
-        print
+        #self.number = self.readNumber(self.address, 13 ,3)
+        #self.number = self.bus.read_i2c_block_data(4,4)[:6]
+        #print "Arduino: Hey RPI, I received a digit ", self.number
+        self.x2= self.bus.read_byte(self.address)
+        self.x3= self.bus.read_byte(self.address)
+        print "recived : ", self.x2, self.x3
+        #~ self.x1= self.bus.read_i2c_block_data(self.address,1)[:1]
+        #~ self.bus.read_byte(self.address)[:6]
+        #~ x2= self.bus.read_byte(self.address)
+        #~ x3= self.bus.read_byte(self.address)
+        #~ x4= self.bus.read_byte(self.address)
+        #~ x5= self.bus.read_byte(self.address)
+        #~ x6= self.bus.read_byte(self.address)
+        
+        #~ print "recived : ", self.x1
   
   #~ def get_addresses(self):
     #~ for strip in xrange(self.max_strips):
--- mrs_motors/setup.py
+++ mrs_motors/setup.py
@@ -5,7 +5,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
-          packages=['takktile_raspberry'],
+          packages=['mrs_motors'],
               package_dir={'': 'scripts'})
 
 setup(**setup_args)
